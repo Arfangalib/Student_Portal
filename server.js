66bd2e9dbd8d8ec7d0f2e950328554a9db0c34c7
Arfangalib <126896464+Arfangalib@users.noreply.github.com>
2024-08-27 18:54:25 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'galibali123',
+    password: '',
     database: 'student_portal'
 });
 
